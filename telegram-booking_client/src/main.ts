db64e6796192373302f9d8157d6caeac0d8ed989
Sh1zome <shizomech@gmail.com>
2023-12-29 03:12:50 +0500

    [FEATURE] add buttons

--- telegram-booking_client/src/main.ts
+++ telegram-booking_client/src/main.ts
@@ -1,19 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { getBotToken } from 'nestjs-telegraf';
+import { text } from 'telegraf/typings/button';
+import { Markup } from 'telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
-  // ...
   const bot = app.get(getBotToken());
 
-  bot.start((ctx) => ctx.reply('Welcome'))
+  bot.start((ctx) => ctx.reply('Welcome', Markup.keyboard([
+    ['➡️ Send $XRP', '📈 Market'],
+    ['⚖️ Balance', '⬆️ Withdraw'],
+    ['🔔 Notificaiton', '👥 Contact']
+  ]).resize()))
   bot.help((ctx) => ctx.reply('Send me a sticker'))
   bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+  bot.on(['sticker'], (ctx) => ctx.reply('👍'))
 
   app.use(bot.webhookCallback('/secret-path'));
 
 }
+
 bootstrap();
