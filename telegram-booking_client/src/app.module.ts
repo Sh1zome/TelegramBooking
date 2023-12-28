59627650e5e2be3f7acb2d16d0287d50ca411c52
Sh1zome <shizomech@gmail.com>
2023-12-29 02:07:07 +0500

    [UPDATE] set client has been launch

--- telegram-booking_client/src/app.module.ts
+++ telegram-booking_client/src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TelegrafModule.forRoot({
+      token: '6543972071:AAEHHUEFZsQdWQ1v9f7erz8xQ06neMPlJ7c',
+    }),
+  ],
 })
+
 export class AppModule {}
--- telegram-booking_client/src/main.ts
+++ telegram-booking_client/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getBotToken } from 'nestjs-telegraf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // ...
+  const bot = app.get(getBotToken());
+
+  bot.start((ctx) => ctx.reply('Welcome'))
+  bot.help((ctx) => ctx.reply('Send me a sticker'))
+  bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+  app.use(bot.webhookCallback('/secret-path'));
+
 }
 bootstrap();
